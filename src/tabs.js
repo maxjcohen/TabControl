0b422655228a0d4f5883cec9f32aecba04b85d06
Max Cohen <max.zagouri@pm.me>
2021-10-17 11:22:48 +0200

    Set default maxOpenTabs to 3

--- src/tabs.js
+++ src/tabs.js
@@ -1,4 +1,4 @@
-let maxOpenTabs = 1;
+let maxOpenTabs = 3;
 let currentTabs = [];
 
 browser.tabs.onCreated.addListener(async (tab) => {
@@ -14,14 +14,17 @@ browser.tabs.onRemoved.addListener(async (tabId, removeInfo) => {
     currentTabs = currentTabs.filter(item => item !== tabId);
 });
 
-function onGet(results) {
-    if (results.maxOpenTabs) {
-	maxOpenTabs = results.maxOpenTabs;
+async function loadMaxOpenTabs() {
+    function onGet(results) {
+	if (results.maxOpenTabs) {
+	    maxOpenTabs = results.maxOpenTabs;
+	}
     }
-}
-function onError(error) {
-    console.log(error);
-}
+    function onError(error) {
+	console.log(error);
+    }
 
-let current = browser.storage.sync.get("maxOpenTabs");
-current.then(onGet, onError);
+    let current = browser.storage.sync.get("maxOpenTabs");
+    current.then(onGet, onError);
+}
+document.addEventListener("DOMContentLoaded", loadMaxOpenTabs);
