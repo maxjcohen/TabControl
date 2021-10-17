c67b29a47811e4021e7ae4c89fc1b4a968ed2490
Guy <guylifshitz@gmail.com>
2021-10-17 15:19:44 +0200

    Working verison handling duplicate tabs

--- src/settings.js
+++ src/settings.js
@@ -1,21 +1,30 @@
 function saveSettings(e) {
-    e.preventDefault();
-    browser.storage.sync.set({
-	maxOpenTabs: document.querySelector("#maxOpenTabs").value
-    });
-    browser.runtime.sendMessage({});
+  e.preventDefault();
+  browser.storage.sync.set({
+    maxOpenTabs: document.querySelector("#maxOpenTabs").value,
+    closeDuplicateTabs: document.querySelector("#closeDuplicateTabs").checked
+  });
+  browser.runtime.sendMessage({});
 }
 
 function restoreSettings() {
-    function setCurrentValue(result) {
-	document.querySelector("#maxOpenTabs").value = result.maxOpenTabs || 3;
-    }
-    function onError(error) {
-	console.log(error);
-    }
+  function setMaxOpenTabs(result) {
+    document.querySelector("#maxOpenTabs").value = result.maxOpenTabs || 3;
+  }
+  function setCloseDuplicateTabs(result) {
+    document.querySelector("#closeDuplicateTabs").checked =
+      result.closeDuplicateTabs | false;
+  }
+  function onError(error) {
+    console.log(error);
+  }
 
-    let current = browser.storage.sync.get("maxOpenTabs");
-    current.then(setCurrentValue, onError);
+  let currentMaxOpenTabs = browser.storage.sync.get("maxOpenTabs");
+  let currentCloseDuplicateTabs =
+    browser.storage.sync.get("closeDuplicateTabs");
+
+  currentMaxOpenTabs.then(setMaxOpenTabs, onError);
+  currentCloseDuplicateTabs.then(setCloseDuplicateTabs, onError);
 }
 
 document.addEventListener("DOMContentLoaded", restoreSettings);
--- src/tabs.js
+++ src/tabs.js
@@ -1,31 +1,73 @@
 let maxOpenTabs = 3;
 let currentTabs = [];
+let displayedMessageTabs = {};
+let closeDuplicateTabs = true;
 
 browser.tabs.onCreated.addListener(async (tab) => {
-    if (tab.type == "messageDisplay") {
-	currentTabs.push(tab.id);
+  if (tab.type == "messageDisplay") {
+    currentTabs.push(tab.id);
+
+    var displayedMessages = await browser.messageDisplay.getDisplayedMessages(
+      tab.id
+    );
+    for (const displayedMessage of displayedMessages) {
+      if (displayedMessageTabs[displayedMessage.id]) {
+        let tabToRemove = displayedMessageTabs[displayedMessage.id];
+        if (closeDuplicateTabs) {
+          browser.tabs.remove(tabToRemove);
+        }
+      }
+      displayedMessageTabs[displayedMessage.id] = tab.id;
     }
     if (currentTabs.length > maxOpenTabs) {
-	browser.tabs.remove(currentTabs.shift())
+      browser.tabs.remove(currentTabs.shift());
     }
+  }
 });
 
 browser.tabs.onRemoved.addListener(async (tabId, removeInfo) => {
-    currentTabs = currentTabs.filter(item => item !== tabId);
+  currentTabs = currentTabs.filter((item) => item !== tabId);
 });
 
-async function loadMaxOpenTabs() {
-    function onGet(results) {
-	if (results.maxOpenTabs) {
-	    maxOpenTabs = results.maxOpenTabs;
-	}
+async function loadVariables() {
+  function onGetMaxOpenTabs(results) {
+    if (results.maxOpenTabs) {
+      maxOpenTabs = results.maxOpenTabs;
     }
-    function onError(error) {
-	console.log(error);
-    }
+  }
 
-    let current = browser.storage.sync.get("maxOpenTabs");
-    current.then(onGet, onError);
+  function onCloseDuplicateTabs(results) {
+    if (results.closeDuplicateTabs != undefined) {
+      closeDuplicateTabs = results.closeDuplicateTabs;
+      console.log(closeDuplicateTabs);
+    }
+  }
+
+  function onError(error) {
+    console.log(error);
+  }
+
+  let currentMaxOpenTabs = browser.storage.sync.get("maxOpenTabs");
+  currentMaxOpenTabs.then(onGetMaxOpenTabs, onError);
+
+  let currentCloseDuplicateTabs =
+    browser.storage.sync.get("closeDuplicateTabs");
+  currentCloseDuplicateTabs.then(onCloseDuplicateTabs, onError);
 }
-document.addEventListener("DOMContentLoaded", loadMaxOpenTabs);
-browser.runtime.onMessage.addListener(loadMaxOpenTabs);
+
+async function loadCloseDuplicateTabs() {
+  function onGet(results) {
+    if (results.closeDuplicateTabs) {
+      closeDuplicateTabs = results.closeDuplicateTabs;
+    }
+  }
+  function onError(error) {
+    console.log(error);
+  }
+
+  let current = browser.storage.sync.get("closeDuplicateTabs");
+  current.then(onGet, onError);
+}
+
+document.addEventListener("DOMContentLoaded", loadVariables);
+browser.runtime.onMessage.addListener(loadVariables);
